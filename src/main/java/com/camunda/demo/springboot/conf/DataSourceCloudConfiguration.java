227bd317c8cb714109cccbcf08d1b7183547c9e4
Ralph Engelmann <ralph@rshc.de>
2019-04-11 09:40:37 +0200

    Fixed double Bean Definition
    Bean "Cloud" is already provided by RabbitMq Cloud Starter

--- src/main/java/com/camunda/demo/springboot/conf/DataSourceCloudConfiguration.java
+++ src/main/java/com/camunda/demo/springboot/conf/DataSourceCloudConfiguration.java
@@ -2,6 +2,7 @@ package com.camunda.demo.springboot.conf;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.Cloud;
 import org.springframework.cloud.CloudFactory;
 import org.springframework.context.annotation.Bean;
@@ -12,14 +13,17 @@ import org.springframework.context.annotation.Profile;
 @Profile("cloud")
 public class DataSourceCloudConfiguration {
 
-    @Bean
-    public Cloud cloud() {
-        return new CloudFactory().getCloud();
-    }
+    @Autowired
+    public Cloud cloud;
+    
+//    @Bean
+//    public Cloud cloud() {
+//        return new CloudFactory().getCloud();
+//    }
 
     @Bean
     public DataSource dataSource() {
-        return cloud().getSingletonServiceConnector(DataSource.class, null);
+        return cloud.getSingletonServiceConnector(DataSource.class, null);
     }
 
 }
